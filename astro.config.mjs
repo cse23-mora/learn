f47a60b780926ab5132bceb3ee142ed64bc8fc0e
Suhas Dissanayake <suhasdissa@gmail.com>
2025-11-06 21:54:46 +0530

    style: update look and feel of the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,23 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeMathJax from 'rehype-mathjax';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import { passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://learn.cse23.org', // Replace with your actual domain
+	image: {
+		service: passthroughImageService(),
+	},
 	markdown: {
 		remarkPlugins: [remarkMath],
 		rehypePlugins: [rehypeMathJax],
 	},
-	integrations: [sitemap()],
+	integrations: [
+		sitemap(),
+	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
